67fbfbf366163598234a908ec7e87547ef919e0f
shinebayar-lab <rnmkrmn@gmail.com>
2020-06-14 22:36:08 -0500

    add /admin page to LoginFilter

--- wap/src/main/java/filter/LoginFilter.java
+++ wap/src/main/java/filter/LoginFilter.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebFilter(filterName = "filter.LoginFilter", urlPatterns = {"/"})
+@WebFilter(filterName = "filter.LoginFilter", urlPatterns = {"/", "/admin"})
 public class LoginFilter implements Filter {
 
     @Override
