bc5abf71f1bbf8772a760b61ee732aadd58cf148
M.Saleh <mega_unknown@hotmail.com>
2020-06-15 01:15:19 -0500

    Change course

--- wap/src/main/java/controller/AdminController.java
+++ wap/src/main/java/controller/AdminController.java
@@ -36,18 +36,15 @@ public class AdminController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/plain");
-
         PrintWriter out = resp.getWriter();
         String action = req.getParameter("action");
-       // int selectedId = Integer.parseInt(req.getParameter("selected_id")) ;
-        String obj ;
 
         if(action.equals("delete"))
         {
-            String selectedId = req.getParameter("course_code");
+            String selectedCourse = req.getParameter("course_code");
             resp.setContentType("text/json");
             //Delete object to the DATABASE
-            CourseDAO.CourseDAO().deleteCourse(selectedId);
+            CourseDAO.CourseDAO().deleteCourse(selectedCourse);
             JsonArray array = Helper.getItemsJSON();
             out.print(array);
         }
@@ -71,7 +68,6 @@ public class AdminController extends HttpServlet {
             String credits = req.getParameter("course_credits");
             String instructor = req.getParameter("course_instructor");
             //Add object to the DATABASE
-
             CourseDAO.CourseDAO().addCourse(new Course(code,name,credits,instructor));
             JsonArray array = Helper.getItemsJSON();
             out.print(array);
