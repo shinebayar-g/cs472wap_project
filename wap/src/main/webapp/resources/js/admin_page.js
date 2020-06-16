cb00cf1719f9fd5165af2ea5d2b7cf60c09878ee
M.Saleh <mega_unknown@hotmail.com>
2020-06-16 12:22:49 -0500

    JS Admin Fix

--- wap/src/main/webapp/resources/js/admin_page.js
+++ wap/src/main/webapp/resources/js/admin_page.js
@@ -1,68 +1,54 @@
+$(function () {
 
-function deleteRow(rowIndex) {
-    let rowData = document.getElementById("tbl").rows[rowIndex];
-    //Create Ajax Call to Delete from the Backend
-    let courseId = rowData.cells[1].innerHTML;
-    deleteAjax(courseId);
-    //Delete From the UI
-    document.getElementById("tbl").deleteRow(rowIndex);
-}
-
-function fetchRowData(rowIndex) {
-    let rowData = document.getElementById("tbl").rows[rowIndex];
-
-    document.getElementById('course_code').value = rowData.cells[1].innerHTML;
-    document.getElementById("course_code").readOnly = true;
-    document.getElementById('course_name').value = rowData.cells[2].innerHTML;
-    document.getElementById('course_credits').value = rowData.cells[3].innerHTML;
-    document.getElementById('course_instructor').value = rowData.cells[4].innerHTML;
-}
-
-$(document).ready(function () {
     function load() {
         $.ajax({
             type: 'GET',
             url: 'admin?type=json',
             success: function (result) {
-                let JSONObjectArray =  result;
-                let table =  document.getElementById("tbl");
-                let rowCount = table.rows.length;
-                for(let i = 0 ; i < JSONObjectArray.length ; i++ )
-                {
-                    let JSONObject = JSONObjectArray[i];
-                    let row = table.insertRow(i+1);
-
-                    let cell0 = row.insertCell(0);
-                    let cell1 = row.insertCell(1);
-                    let cell2 = row.insertCell(2);
-                    let cell3 = row.insertCell(3);
-                    let cell4 = row.insertCell(4);
-                    let cell5 = row.insertCell(5);
-                    let cell6 = row.insertCell(6);
-
-                    cell0.innerHTML = (i+1);
-                    cell1.innerHTML = JSONObject.code;
-                    cell2.innerHTML = JSONObject.name;
-                    cell3.innerHTML = JSONObject.credit;
-                    cell4.innerHTML = JSONObject.instructor;
-
-                    /**************************** Edit Button *****************************/
-                    var editButton = document.createElement('input');
-                    editButton.setAttribute('type', 'button');
-                    editButton.setAttribute('value', 'Edit');
-                    editButton.setAttribute('onclick', 'fetchRowData('+ (i+1) +')');
-                    cell5.appendChild(editButton);
-                    /**************************** Delete Button *****************************/
-                    var deleteButton = document.createElement('input');
-                    deleteButton.setAttribute('type', 'button');
-                    deleteButton.setAttribute('value', 'Delete');
-                    deleteButton.setAttribute('onclick', 'deleteRow('+ (i+1) +')');
-                    cell6.appendChild(deleteButton);
-                }
+                reload(result);
             }
         });
     }
+
     load();
+});
+
+    function reload(result) {
+        let JSONObjectArray = result;
+        let table = document.getElementById("tbl");
+        let rowCount = table.rows.length;
+        for (let i = 0; i < JSONObjectArray.length ; i++) {
+            let JSONObject = JSONObjectArray[i];
+            let row = table.insertRow(i + 1);
+
+            let cell0 = row.insertCell(0);
+            let cell1 = row.insertCell(1);
+            let cell2 = row.insertCell(2);
+            let cell3 = row.insertCell(3);
+            let cell4 = row.insertCell(4);
+            let cell5 = row.insertCell(5);
+            let cell6 = row.insertCell(6);
+
+            cell0.innerHTML = (i + 1);
+            cell1.innerHTML = JSONObject.code;
+            cell2.innerHTML = JSONObject.name;
+            cell3.innerHTML = JSONObject.credit;
+            cell4.innerHTML = JSONObject.instructor;
+
+            /**************************** Edit Button *****************************/
+            var editButton = document.createElement('input');
+            editButton.setAttribute('type', 'button');
+            editButton.setAttribute('value', 'Edit');
+            editButton.setAttribute('onclick', 'fetchRowData(' + (i + 1) + ')');
+            cell5.appendChild(editButton);
+            /**************************** Delete Button *****************************/
+            var deleteButton = document.createElement('input');
+            deleteButton.setAttribute('type', 'button');
+            deleteButton.setAttribute('value', 'Delete');
+            deleteButton.setAttribute('onclick', 'deleteRow(' + (i + 1) + ')');
+            cell6.appendChild(deleteButton);
+        }
+    }
 
     /*
    * Clear Form Content
@@ -75,6 +61,8 @@ $(document).ready(function () {
         document.getElementById('course_instructor').value = "";
     })
 
+/* Add Button Handler
+* */
     $('#btnAdd').click(function () {
         //alert("Adding new...")
         let code = $('#course_code').val();
@@ -89,41 +77,11 @@ $(document).ready(function () {
                 course_name: name,
                 course_credits: credit,
                 course_instructor: instructor,
-                action: 'add'},
+                action: 'add'
+            },
             url: 'admin',
             success: function (result) {
-                let JSONObject = result;
-                let table =  document.getElementById("tbl");
-                let rowCount = table.rows.length;
-
-
-                let row = table.insertRow(rowCount);
-                let cell0 = row.insertCell(0);
-                let cell1 = row.insertCell(1);
-                let cell2 = row.insertCell(2);
-                let cell3 = row.insertCell(3);
-                let cell4 = row.insertCell(4);
-                let cell5 = row.insertCell(5);
-                let cell6 = row.insertCell(6);
-
-                cell0.innerHTML = (i+1);
-                cell1.innerHTML = JSONObject.code;
-                cell2.innerHTML = JSONObject.name;
-                cell3.innerHTML = JSONObject.credit;
-                cell4.innerHTML = JSONObject.instructor;
-
-                /**************************** Edit Button *****************************/
-                let editButton = document.createElement('input');
-                editButton.setAttribute('type', 'button');
-                editButton.setAttribute('value', 'Edit');
-                editButton.setAttribute('onclick', 'fetchRowData('+ (i+1) +');');
-                cell5.appendChild(editButton);
-                /**************************** Delete Button *****************************/
-                let deleteButton = document.createElement('input');
-                deleteButton.setAttribute('type', 'button');
-                deleteButton.setAttribute('value', 'Delete');
-                deleteButton.setAttribute('onclick', 'deleteRow('+ (i+1) +');');
-                cell6.appendChild(deleteButton);
+                reload(result);
             }
         });
 
@@ -131,6 +89,9 @@ $(document).ready(function () {
         window.location.reload();
     });
 
+    /*
+    * Save Edit Handler
+    * */
     $('#btnSaveEdit').click(function () {
         //course_code course_name course_credits course_instructor
 
@@ -151,65 +112,50 @@ $(document).ready(function () {
             },
             url: 'admin',
             success: function (result) {
-                //alert(result);
-                document.getElementById("course_code").readOnly = false;
-                let JSONObjectArray =  result;
-                let table =  document.getElementById("tbl");
-                let rowCount = document.getElementById("tbl").rows.length;
-                for(let i = 0 ; i < JSONObjectArray.length ; i++ )
-                {
-                    let JSONObject = JSONObjectArray[i];
-                    let row = table.insertRow(i+1);
-                    let cell0 = row.insertCell(0);
-                    let cell1 = row.insertCell(1);
-                    let cell2 = row.insertCell(2);
-                    let cell3 = row.insertCell(3);
-                    let cell4 = row.insertCell(4);
-                    let cell5 = row.insertCell(5);
-                    let cell6 = row.insertCell(6);
-
-                    cell0.innerHTML = (i+1);
-                    cell1.innerHTML = JSONObject.code;
-                    cell2.innerHTML = JSONObject.name;
-                    cell3.innerHTML = JSONObject.credit;
-                    cell4.innerHTML = JSONObject.instructor;
-
-                    /**************************** Add Button *****************************/
-                    var editButton = document.createElement('input');
-                    editButton.setAttribute('type', 'button');
-                    editButton.setAttribute('value', 'Edit');
-                    editButton.setAttribute('onclick', 'fetchRowData('+ (i+1) +')');
-                    cell5.appendChild(editButton);
-                    /**************************** Delete Button *****************************/
-                    var deleteButton = document.createElement('input');
-                    deleteButton.setAttribute('type', 'button');
-                    deleteButton.setAttribute('value', 'Delete');
-                    deleteButton.setAttribute('onclick', 'deleteRow('+ (i+1) +')');
-                    cell6.appendChild(deleteButton);
-                }
-
+                reload(result);
             }
         });
         window.location.reload();
         //re build the UI elements=> Buttons
         //location.reload();
     });
-});
 
-function deleteAjax(courseId) {
-    $.ajax({
-        type: 'POST',
-        url: 'admin',
-        data: {
-            course_code: courseId,
-            action: 'delete'
-        },
-        success: function (result) {
-            //    alert("Return Result from Delete :" + result);
-            load();
-        }
-    });
-}
+    function deleteRow(rowIndex) {
+        let rowData = document.getElementById("tbl").rows[rowIndex];
+        //Create Ajax Call to Delete from the Backend
+        let courseId = rowData.cells[1].innerHTML;
+        deleteAjax(courseId);
+        //Delete From the UI
+        document.getElementById("tbl").deleteRow(rowIndex);
+    }
+
+    function fetchRowData(rowIndex) {
+        let rowData = document.getElementById("tbl").rows[rowIndex];
+
+        document.getElementById('course_code').value = rowData.cells[1].innerHTML;
+        document.getElementById("course_code").readOnly = true;
+        document.getElementById('course_name').value = rowData.cells[2].innerHTML;
+        document.getElementById('course_credits').value = rowData.cells[3].innerHTML;
+        document.getElementById('course_instructor').value = rowData.cells[4].innerHTML;
+    }
+
+    function deleteAjax(courseId) {
+        $.ajax({
+            type: 'POST',
+            url: 'admin',
+            data: {
+                course_code: courseId,
+                action: 'delete'
+            },
+            success: function (result) {
+                //    alert("Return Result from Delete :" + result);
+                reload();
+            }
+        });
+    }
+
+
+
 
 
 
